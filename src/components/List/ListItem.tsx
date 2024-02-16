46ecec439bf8d43f8056741b30da3d737861a512
berkaysson <berkaysonel85@gmail.com>
2024-02-16 22:20:43 +0300

    Implement show/hide functionality in ListItem component

--- src/components/List/ListItem.tsx
+++ src/components/List/ListItem.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Country } from "../../models/Country";
 
 interface ListItemProps {
@@ -8,40 +8,58 @@ interface ListItemProps {
 }
 
 const ListItem: React.FC<ListItemProps> = ({ country, checked, onToggle }) => {
+  const [showDetails, setShowDetails] = useState<boolean>(false);
+
   return (
-    <li onClick={() => onToggle(country.code)} style={{ cursor: "pointer" }}>
+    <li>
       <input
         type="checkbox"
         checked={checked === country.code}
         onChange={() => onToggle(country.code)}
       />
-      <div>
-        <strong>Name:</strong> {country.name}
+      <div onClick={() => onToggle(country.code)} style={{ cursor: "pointer" }}>
+        <div>
+          <strong>Name:</strong> {country.name} <strong>Code:</strong>{" "}
+          {country.code}
+        </div>
+        {showDetails ? (
+          <>
+            <div>
+              <strong>Continent:</strong> {country.continent?.name} (
+              {country.continent?.code})
+            </div>
+            <div>
+              <strong>Currency:</strong> {country.currency}
+            </div>
+            <div>
+              <strong>Emoji:</strong> {country.emoji}
+            </div>
+            <div>
+              <strong>Languages:</strong>{" "}
+              {country.languages
+                ?.map((language) => `${language.name} (${language.code})`)
+                .join(", ")}
+            </div>
+            <div>
+              <strong>Native:</strong> {country.native}
+            </div>
+            <div>
+              <strong>Phone:</strong> {country.phone}
+            </div>
+            <div>
+              <strong>States:</strong>{" "}
+              {country.states
+                ?.map((state) => `${state.name} (${state.code})`)
+                .join(", ")}
+            </div>
+          </>
+        ) : (
+          ""
+        )}
       </div>
-      <div>
-        <strong>Code:</strong> {country.code}
-      </div>
-      <div>
-        <strong>Continent:</strong> {country.continent?.name} ({country.continent?.code})
-      </div>
-      <div>
-        <strong>Currency:</strong> {country.currency}
-      </div>
-      <div>
-        <strong>Emoji:</strong> {country.emoji}
-      </div>
-      <div>
-        <strong>Languages:</strong> {country.languages?.map(language => `${language.name} (${language.code})`).join(', ')}
-      </div>
-      <div>
-        <strong>Native:</strong> {country.native}
-      </div>
-      <div>
-        <strong>Phone:</strong> {country.phone}
-      </div>
-      <div>
-        <strong>States:</strong> {country.states?.map(state => `${state.name} (${state.code})`).join(', ')}
-      </div>
+      <button onClick={() => setShowDetails(!showDetails)}>
+        {showDetails ? "Hide" : "Show"} Details
+      </button>
     </li>
   );
 };
